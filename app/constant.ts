d562d3580b3f2658e3e21bd2de9fdd65670604a4
withsalt <sysrun@163.com>
2025-08-06 10:07:35 +0800

    新增ddepseek

--- app/constant.ts
+++ app/constant.ts
@@ -598,6 +598,9 @@ const bytedanceModels = [
   "Doubao-pro-4k",
   "Doubao-pro-32k",
   "Doubao-pro-128k",
+  "doubao-1-5-pro-256k-250115",
+  "deepseek-r1-250528",
+  "deepseek-v3-250324",
 ];
 
 const alibabaModes = [
